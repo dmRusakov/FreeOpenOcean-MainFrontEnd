cf440991e2351fb6af6eb6a38988543608e739f8
Dmitrii Rusakov <78332305+dmRusakov@users.noreply.github.com>
2026-01-18 17:18:51 -0500

    get backend data

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -2,13 +2,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:free_open_ocean/pages/page_template.dart';
 import 'package:free_open_ocean/core/localization/AppLocalizations.dart';
-import 'package:free_open_ocean_grpc/src/grpc/status/v1/status.pbgrpc.dart' show StatusClient;
-import 'package:free_open_ocean_grpc/src/grpc/status/v1/status.pb.dart' show GetRequest;
 import 'package:flutter/foundation.dart';
-import 'package:grpc/grpc.dart';
-import 'package:http/http.dart' as http;
-
-const String statusGetPath = '/status.v1.Status/Get';
+import 'package:free_open_ocean/services/status.dart';
 
 class UserPage extends StatefulWidget {
   final Map<String, String>? params;
@@ -21,6 +16,7 @@ class UserPage extends StatefulWidget {
 
 class _UserPageState extends State<UserPage> {
   String statusResult = 'Loading status...';
+  final _statusService = StatusService();
 
   @override
   void initState() {
@@ -29,52 +25,10 @@ class _UserPageState extends State<UserPage> {
   }
 
   Future<void> _callStatus() async {
-    try {
-      if (kIsWeb) {
-        // Use HTTP for web with JSON, matching the unit test
-        final url = Uri.parse('http://localhost:8081/status.v1.Status/Get');
-        final response = await http.post(
-          url,
-          headers: {'Content-Type': 'application/json'},
-          body: '{}',
-        );
-
-        if (response.statusCode == 200) {
-          // Parse the JSON response
-          final data = jsonDecode(response.body);
-          final id = data['id'];
-          final name = data['name'];
-          final loads = data['loads'];
-          setState(() {
-            statusResult = 'ID: $id, Name: $name, Loads: $loads%';
-          });
-        } else {
-          setState(() {
-            statusResult = 'Error: HTTP ${response.statusCode}';
-          });
-        }
-      } else {
-        // Existing gRPC logic for Android and Linux
-        final host = '10.0.2.2';
-        final port = 50051;
-        final channel = ClientChannel(
-          host,
-          port: port,
-          options: ChannelOptions(credentials: ChannelCredentials.insecure()),
-        );
-        final client = StatusClient(channel);
-        final request = GetRequest();
-        final response = await client.get(request);
-        setState(() {
-          statusResult = 'ID: ${response.id}, Name: ${response.name}, Loads: ${response.loads}%';
-        });
-        await channel.shutdown();
-      }
-    } catch (e) {
-      setState(() {
-        statusResult = 'Error: $e';
-      });
-    }
+    final result = await _statusService.getStatus();
+    setState(() {
+      statusResult = result;
+    });
   }
 
   @override
